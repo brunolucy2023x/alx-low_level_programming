8d0751a7fc6bdb5093327272abdba3cdcdfa0741
brunolucy2023x <lucyb5147@gmail.com>
2024-02-02 02:26:01 -0800

    Final Update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * bruno okoth
- * brunookoth44@gmail.com
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
