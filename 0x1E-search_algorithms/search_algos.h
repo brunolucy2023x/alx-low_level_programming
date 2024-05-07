dd70fdf850bfe75f9bff6df91bb25130248fdf7b
brunolucy2023x <lucyb5147@gmail.com>
2024-05-07 16:46:53 -0700

    twenty

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,15 +1,54 @@
-#ifndef _search_algos_
-#define _search_algos_
-
-#include <stddef.h>
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+/* struct for task 12 */
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/* struct for task 13 */
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+/* functions for mandatory tasks */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, size_t l, size_t r);
-int binary_search_index(int *array, size_t l, size_t r, int value);
 
-int binary_search(int *array, size_t size, int value);
+/* functions for advanced tasks */
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
@@ -17,5 +56,4 @@ int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
-
-#endif /* _search_algos_ */
+#endif /* SEARCH_ALGOS_H */
